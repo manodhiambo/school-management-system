3e7fcdadb5fe695fbb651e503dcf19d2c38b6dd0
manodhiambo <manodhiambo@gmail.com>
2025-12-12 08:18:19 +0300

    fix

--- services/backend/src/routes/libraryRoutes.js
+++ services/backend/src/routes/libraryRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import libraryController from '../controllers/libraryController.js';
+import * as libraryController from '../controllers/libraryController.js';
 import { authenticate, authorize } from '../middleware/authMiddleware.js';
 
 const router = express.Router();

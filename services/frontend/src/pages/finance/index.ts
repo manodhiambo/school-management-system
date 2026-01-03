45777f551a552c2e6683ee82be5e5b5476cb258e
manodhiambo <manodhiambo@gmail.com>
2026-01-03 13:59:18 +0300

    Fix finance index.ts to export new BudgetManagement and VendorsPurchaseOrders components

--- services/frontend/src/pages/finance/index.ts
+++ services/frontend/src/pages/finance/index.ts
@@ -1,7 +1,7 @@
 export { default as FinanceDashboard } from './FinanceDashboard';
 export { default as Transactions } from './Transactions';
-export { default as Budgets } from './Budgets';
-export { default as Vendors } from './Vendors';
+export { default as Budgets } from './BudgetManagement';
+export { default as Vendors } from './VendorsPurchaseOrders';
 export { default as BankAccounts } from './BankAccounts';
 export { default as PettyCash } from './PettyCash';
 export { default as Assets } from './Assets';
